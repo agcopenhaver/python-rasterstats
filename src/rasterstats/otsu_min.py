0328818d57b0d9aa665ef7e294070533096b4c1c
Andrew Copenhaver <agcopenhaver@gwmail.gwu.edu>
2019-03-22 14:49:33 -0400

    Update otsu_min.py

--- src/rasterstats/otsu_min.py
+++ src/rasterstats/otsu_min.py
@@ -3,10 +3,13 @@ from skimage.filters import threshold_otsu
 
 def otsu_min(y):
     y = y.compressed()
+    if y.min() == y.max():
+        return y.min()
     y =  np.ma.masked_where(y < 1, y)
     thresh = threshold_otsu(y)
     super_threshold_indices = y < thresh
     y[super_threshold_indices] = 0
     y = y[np.nonzero(y)]
     return np.min(y)
+
 
